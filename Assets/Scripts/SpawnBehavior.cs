a48c539d47f9f1a63d47e0c78d8c1cee7da1a777
Daniel <76703548+WingmanDaniel@users.noreply.github.com>
2022-04-27 21:34:35 -0600

    Spawn Object Created
    TaskOne:
    Modification:
    1. created a spawn object.
    2. created two scripts: SpawnBehavior.cs and SphereBehavior.cs
    Effect:
    a sphere will fall down to the box after starting

---
+++ Assets/Scripts/SpawnBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBehavior : MonoBehaviour
+{
+    public GameObject sphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnSphere();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void spawnSphere()
+    {
+        var pos = new Vector3(Random.Range(-0.75f, 0.75f), 2.0f, Random.Range(-0.75f, 0.75f));
+        Instantiate(sphere, pos, Quaternion.identity);
+    }
+}
